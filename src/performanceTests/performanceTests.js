95933a92b146d9d843f7a5d63bf77405d5d35184
david limkys <david.limkys@gmail.com>
2017-03-02 23:40:05 +0200

    Reenabled some tests.

--- src/performanceTests/performanceTests.js
+++ src/performanceTests/performanceTests.js
@@ -34,72 +34,72 @@ fs.readFile('englishWords', 'utf8', function(err, data) {
     test('Add 0.5Mil words to trie', initializeTrie);
 
     test('Add 0.5Mil words to minimalWordGraph', initializeMinimalWordGraph);
-    //
-    // test('Compute trie size', () => {
-    //     console.log('Trie size: ' + trie.size());
-    // });
-    //
-    // test('Compute mwg size', () => {
-    //     console.log('Mwg size: ' + minimalWordGraph.size());
-    // });
-    //
-    // test('Look up long word in trie', () => {
-    //     trie.lookup('unconsentaneousness');
-    // });
-    //
-    // test('Look up long word in mwg', () => {
-    //     minimalWordGraph.lookup('unconsentaneousness');
-    // });
-    //
-    // test('Trie Starts with', () => {
-    //     trie.startsWith('pop');
-    // });
-    //
-    // test('Mwg Starts with', () => {
-    //     trie.startsWith('pop');
-    // });
-    //
-    // test('Trie Ends with', () => {
-    //     trie.endsWith('pop');
-    // });
-    //
-    // test('Mwg Ends with', () => {
-    //     trie.endsWith('pop');
-    // });
-    //
-    // test('QueryBuilder Trie startsWith, endsWith, containsAny', () => {
-    //     new lib.QueryBuilder(trie)
-    //         .startsWith('o')
-    //         .endsWith('zo')
-    //         .containsAny('ya')
-    //         .build()();
-    // });
-    //
-    // test('QueryBuilder Mwg startsWith, endsWith, containsAny', () => {
-    //     new lib.QueryBuilder(trie)
-    //         .startsWith('o')
-    //         .endsWith('zo')
-    //         .containsAny('ya')
-    //         .build()();
-    // });
-    //
-    // test('Minimize trie, affectivly transforming it to a dawg.', () => {
-    //     trie.minimize();
-    // });
-    //
-    // test('Edit distance Trie.', () => {
-    //     lib.editDistance(trie.root, 'festivally', 2);
-    // });
-    //
-    // test('Edit distance MWG.', () => {
-    //     lib.editDistance(minimalWordGraph.root, 'festivally', 2);
-    // });
 
-    test('Edit distance Trie, Max results set to 3.', () => {
-        trie.similarTo('relative', {maxDistance:2, maxResults : 3});
+    test('Compute trie size', () => {
+        console.log('Trie size: ' + trie.size());
     });
 
-    test('Edit distance MWG. Max results set to 3', () => {
-        console.log(minimalWordGraph.similarTo('relative', {maxDistance:2, maxResults : 3}))
+    test('Compute mwg size', () => {
+        console.log('Mwg size: ' + minimalWordGraph.size());
+    });
+
+    test('Look up long word in trie', () => {
+        trie.lookup('unconsentaneousness');
+    });
+
+    test('Look up long word in mwg', () => {
+        minimalWordGraph.lookup('unconsentaneousness');
+    });
+
+    test('Trie Starts with', () => {
+        trie.startsWith('pop');
+    });
+
+    test('Mwg Starts with', () => {
+        trie.startsWith('pop');
+    });
+
+    test('Trie Ends with', () => {
+        trie.endsWith('pop');
+    });
+
+    test('Mwg Ends with', () => {
+        trie.endsWith('pop');
+    });
+
+    test('QueryBuilder Trie startsWith, endsWith, containsAny', () => {
+        new lib.QueryBuilder(trie)
+            .startsWith('o')
+            .endsWith('zo')
+            .containsAny('ya')
+            .build()();
+    });
+
+    test('QueryBuilder Mwg startsWith, endsWith, containsAny', () => {
+        new lib.QueryBuilder(trie)
+            .startsWith('o')
+            .endsWith('zo')
+            .containsAny('ya')
+            .build()();
+    });
+
+    test('Minimize trie, affectivly transforming it to a dawg.', () => {
+        trie.minimize();
+    });
+
+    test('Edit distance Trie.', () => {
+        lib.editDistance(trie.root, 'festivally', 2);
+    });
+
+    test('Edit distance MWG.', () => {
+        lib.editDistance(minimalWordGraph.root, 'festivally', 2);
+    });
+
+    test('Edit distance Trie, Max results set to 3, searching words similar to "pop".', () => {
+        trie.similarTo('pop', {maxDistance:2, maxResults : 3});
+    });
+
+    test('Edit distance MWG. Max results set to 3, searching words similar to "pop".', () => {
+        console.log(minimalWordGraph.similarTo('pop', {maxDistance:2, maxResults : 3}))
     });
 });
